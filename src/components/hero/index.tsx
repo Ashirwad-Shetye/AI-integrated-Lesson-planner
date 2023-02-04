40d3c0254f3b97034184b77cf20ed52d9fca6615
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-05 04:22:20 +0530

    image added

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,24 +1,31 @@
+import plan from "../../assets/images/plan.png";
+
 function Hero() {
   return (
     <div className="relative h-3/6">
-      <div className="relative h-80">
-        <div
-          className="relative m-10 h-64 md:h-72 flex flex-col justify-center bg-white space-y-5 font-roboSlab w-10/12
-                    border-2 border-black lg:w-6/12 z-50"
-        >
-          <div className="text-4xl md:text-5xl mx-5">
-            <h1>Quicker way to make Lesson Plans.</h1>
-            <h1>So you can focus more vital tasks.</h1>
+      <div className="h-24"></div>
+      <div className="flex">
+        <div className="relative h-80 w-full lg:w-8/12">
+          <div
+            className="relative mx-10 h-80 md:h-72 flex flex-col justify-center bg-white space-y-5 font-roboSlab w-10/12 lg:w-8/12
+                    border-2 border-black  z-50"
+          >
+            <div className="text-4xl md:text-5xl mx-5">
+              <h1>Quicker way to make Lesson Plans.</h1>
+              <h1>So you can focus more vital tasks.</h1>
+            </div>
+            <p className="text-md font-bold tracking-wide text-purple-600 mx-5">
+              Use AI generation supported by OpenAI
+            </p>
           </div>
-          <p className="text-md font-bold tracking-wide text-purple-600 mx-5">
-            Use AI generation supported by OpenAI
-          </p>
+          <div
+            className={`h-80 md:h-72 absolute top-2 left-2 mx-10 bg-black w-10/12 lg:w-8/12 z-0`}
+          ></div>
         </div>
-        <div
-          className={`h-64 md:h-72 absolute top-2 left-2 mx-10 bg-black w-10/12 lg:w-6/12 z-0`}
-        ></div>
+        <div className="hidden lg:block w-fit border-2">
+          <img src={plan} alt="planning" className="w-72" />
+        </div>
       </div>
-      <div></div>
     </div>
   );
 }
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -3,7 +3,7 @@ import { BsAsterisk } from "react-icons/bs";
 
 function Nav() {
   return (
-    <div className="bg-yellow-200 h-16 flex items-center justify-between border-b-4 border-black">
+    <div className="bg-yellow-200 w-full fixed h-16 flex items-center justify-between border-b-4 border-black z-50">
       <div className="mx-10">
         <BsAsterisk className="text-3xl" />
       </div>
